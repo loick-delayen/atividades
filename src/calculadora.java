c824f815475fcf5bb81f63c3ef37d76d01a7bea6
Loick <loick.delayen@ba.estudante.senai.br>
2024-02-05 19:44:26 -0300

    Seu commit message aqui

--- src/calculadora.java
+++ src/calculadora.java
@@ -1,3 +1,13 @@
-public class calculadora {
-    
+class Calculadora {
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Calculadora minhaCalculadora = new Calculadora();
+
+        int resultado = minhaCalculadora.somar(1800, 99);
+        System.out.println("soma = " + resultado);
+
+    } 
 }
